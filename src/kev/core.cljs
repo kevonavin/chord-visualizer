86570f3c7f84bb41d8a20ef9287c41fe45f9abb0
kkrausse <kevin.krausse@ekata.com>
2021-10-09 12:19:35 -0700

    full odoyle rules implementation

--- src/kev/core.cljs
+++ src/kev/core.cljs
@@ -1,11 +1,11 @@
 (ns kev.core
   (:require
-   [datascript.core :as d]
    [net.cgrand.xforms :as x]
    [reagent.core :as r]
    [reagent.dom :as rdom]
    [reagent.ratom :as ratom]
-   [odoyle.rules :as o])
+   [odoyle.rules :as o]
+   [cljs.pprint :as pprint])
   (:require-macros [odoyle.rules :as o]))
 
 (defn ezip
@@ -25,9 +25,15 @@
 
 (def fret-multiplier (Math/pow 0.5 (/ 1 12)))
 
-(defn fret-height [fret]
-  (- (Math/pow fret-multiplier (dec fret))
-     (Math/pow fret-multiplier fret)))
+(defn fret-height
+  ([fret]
+   (- (Math/pow fret-multiplier (dec fret))
+      (Math/pow fret-multiplier fret)))
+  ([fret num-frets]
+   (/ (fret-height fret)
+      (- 1 (Math/pow fret-multiplier num-frets)))))
+
+(def notes '("a" "a#" "b" "c" "c#" "d" "d#" "e" "f" "f#" "g" "g#"))
 
 ;; insert a fact
 ;; domid {:reagent/fnum 'stuff :reagent/atom ratom}
@@ -35,37 +41,55 @@
 ;; (->make-rule {:my-param 'fnum} [...query])
 (def rules
   (o/ruleset
-   {::test
+   {::fret-dots
+    [:what
+     [fid :fret/number fnum]
+     :then
+     (let [dots
+           (nth (cycle [0 0 0 1 0 1 0 1 0 1 0 0 2]) fnum)]
+       (o/insert! fid :fret/dots dots))]
+
+    ::fret-notes
+    [:what
+     [fid :fret/number fnum]
+     [::fretboard :fretboard/string-seq string-seq]
+     [::fretboard :fretboard/enabled-notes enabled-notes]
+     :then
+     (o/insert!
+      fid :fret/notes
+      (for [{:keys [note]} string-seq]
+        (->> notes
+             (cycle)
+             (drop-while (partial not= note))
+             (drop fnum)
+             first
+             (enabled-notes))))]
+
+    ::fret-height
     [:what
      [::fretboard :fretboard/height fbh]
      [fid :fret/number fnum]
-     [cid ::fnum fnum]
-     [cid ::ratom ratom]
      :then
-     (reset! ratom {:fnum fnum
-                    :fh (* fbh (fret-height (inc fnum)))})
-     (prn "fret rule fired" {:fnum fnum
-                             :fbh fbh})]
+     (o/insert! fid :fret/height (* fbh (fret-height (inc fnum))))]
 
-    #_#_::fret-height
-      [:what
-       [::fretboard :fretboard/height fbh]
-       [fid :fret/number fnum]
-       :then
-       (prn "fh fired")
-       (o/insert! fid :fret/height (* fbh (fret-height (inc fnum))))]
+    ::string-notes
+    [:what
+     [id :string/number num]
+     [id :string/note note]
+     :then-finally
+     (->> (o/query-all o/*session* ::string-notes)
+          (sort-by :num)
+          (o/insert! ::fretboard :fretboard/string-seq))]
 
     ::fret-seq
     [:what
      [fid :fret/number fnum]
      :then-finally
      (->> (o/query-all o/*session* ::fret-seq)
-          (#(do (prn {:fret-seq2-rule %}) %))
           (transduce (comp
                       (x/sort-by :fnum)
                       (map :fid))
                      conj [])
-          (#(do (prn {:fret-seq2-post %}) %))
           (o/insert! ::fretboard :fretboard/fret-seq))]
 
     ::num-frets
@@ -73,7 +97,6 @@
      [::fretboard :fretboard/num-frets num-frets]
      [::fretboard :fretboard/fret-seq fseq {:then not=}]
      :then
-     (prn "fseq" fseq)
      (doseq [[fnum fsf] (ezip (range num-frets) fseq)]
        (cond
          (nil? fsf)
@@ -81,10 +104,72 @@
          (nil? fnum)
          (o/retract! fsf :fret/number)))]}))
 
-(def initial-facts [[::fretboard {:fretboard/num-frets 20
-                                  :fretboard/height  850
-                                  :fretboard/width 100
-                                  :fretboard/fret-seq nil}]])
+;; interview
+;; say you have a database of items of the form [id attr value]
+;;
+;; e.g. you
+;; write a query language that accepts AND queries
+;; looking like
+;; [?x :painting-id ?y]
+;; [?y :year 1917]
+#_(def *db*
+    [[1 :name "joshua reynolds"]
+     [1 :last-painting 2]
+     [1 :birth-date 1697]
+     [2 :painting-name "big sur"]])
+
+(defn do-query [db q])
+
+(defn params-for [[in & remains :as _query]]
+  (when (= :in in)
+    (->> remains
+         (take-while (complement keyword?)))))
+
+(defn ->pquery-rule
+  "create a parameterized query rule. This can be used later with pquery rname.
+  rname is a qualified keyword. params are a list of params as symbols to be used.
+  These are meant to match up to things in the what clause probably."
+  [rname [in & remains :as query]]
+  (let [params (params-for query)
+        query' (if (= :in in)
+                 (drop-while symbol? remains)
+                 query)]
+    (o/->rule
+     rname
+     (-> (into [] (take 1 query')) ;; due to perf issues, we gotta do this first
+         (into (into [['pquery-aux-id :pquery/pquery-rname rname]
+                      ['pquery-aux-id :pquery/pquery-aux-atom 'pquery-aux-atom]]
+                     (map (fn [p]
+                            ['pquery-aux-id (keyword "pquery" p) p]))
+                     params))
+         (into (drop 1 query'))
+         (into [:then
+                (fn [{:keys [pquery-aux-atom] :as match}]
+                  (reset! pquery-aux-atom (dissoc match
+                                                  :pquery-aux-atom)))])
+         (doto cljs.pprint/pprint)))))
+
+(defn setup-query [sesh rule-name id ratom param-map]
+  (let [fax (merge param-map
+                   {:pquery-rname rule-name
+                    :pquery-aux-atom ratom})
+        fax' (into {}
+                   (map (fn [[k v]]
+                          [(keyword "pquery" k) v]))
+                   fax)]
+    (o/insert sesh id fax')))
+
+(def initial-facts (into [[::fretboard {:fretboard/num-frets 20
+                                        :fretboard/height  850
+                                        :fretboard/width 100
+                                        :fretboard/fret-seq nil
+                                        :fretboard/enabled-notes #{}}]]
+                         (map-indexed
+                          (fn [i n]
+                            [(random-uuid)
+                             {:string/number i
+                              :string/note n}])
+                          ["e" "a" "d" "g" "b" "e"])))
 
 (defn insert-facts [sesh facts]
   (reduce #(apply o/insert %1 %2) sesh facts))
@@ -95,169 +180,202 @@
           (zipmap (repeat id) attrs)))
 
 (def sesh (atom (as-> (o/->session) $
-                 (reduce o/add-rule $ rules)
-                 (insert-facts $ initial-facts)
-                 (o/fire-rules $))))
+                  (reduce o/add-rule $ rules)
+                  (insert-facts $ initial-facts)
+                  (o/fire-rules $))))
 
-(defn rule-react [sesh fact-map]
+(defn maybe-add-rule [sesh rule-name query]
+  (if (get-in sesh [:rule-name->node-id rule-name])
+    sesh
+    (as-> sesh $
+      (o/add-rule $ (->pquery-rule rule-name query))
+      (reduce o/insert $ (o/query-all $))
+      (o/fire-rules $))))
+
+(defn rule-react2 [rule-name query sesh & args]
   (r/with-let [atm (r/atom nil)
                id (random-uuid)
-               _ (swap! sesh insert-facts
-                        [[id (assoc fact-map
-                                    ::ratom atm)]])
+               param-map (zipmap (params-for query)
+                                 args)
+               _ (swap! sesh maybe-add-rule rule-name query)
+               _ (swap! sesh
+                        setup-query
+                        rule-name
+                        id
+                        atm
+                        param-map)
                _ (swap! sesh o/fire-rules)]
     @atm
     (finally
-      (swap! sesh remove-facts id (into [::ratom]
-                                        (keys fact-map))))))
+      (swap! sesh remove-facts id (map (fn [[k _v]]
+                                         (keyword "pquery" k))
+                                       param-map)))))
 
-(def schema {:fretboard/height {:db/valueType :db.type/ref}
-             :fretboard/width {:db/valueType :db.type/long}
-             :fret/ndots {:db/valueType :db.type/long}
-             :fret/number {:db/valueType :db.type/long}})
-
-(def conn (d/create-conn))
-
-;; see posh https://github.com/denistakeda/posh
-;; which implements this, probably better than I
-(d/transact! conn [{:db/ident :fretboard
-                    :fretboard/height 850
-                    :fretboard/width 100}])
-
-(d/transact! conn (->> [0 0 1 0 1 0 1 0 1 0 0 2]
-                       (cycle)
-                       (map-indexed (fn [idx dots]
-                                      {:fret/number idx
-                                       :db/ident idx
-                                       :fret/ndots  dots}))
-                       (take 20)
-                       (into [])))
-
-(defn reactq [conn q & {:keys [xform in]}]
-  (letfn [(runq [db]
-            (->> (apply d/q q db in)
-                 (into [] (or xform identity))
-                 first))]
-    (r/with-let [ratom (r/atom (runq @conn))
-                 lid (d/listen! conn
-                                (fn [{:keys [db-before db-after] :as _tx-report}]
-                                  (reset! ratom (runq db-after))))]
-      @ratom
-      (finally
-        (d/unlisten! conn lid)))))
-
-(defn fret [w h marks notes]
+(defn fret [w h dots notes {:keys [no-border?]}]
   (let [mark-height (/ h 3.5)
-        key-height (/ w 5)
-        inner-style {:height          h
-                     :width           w
-                     :border          "0.5px solid black"
+        border-bottom (if no-border? 0 1)
+        key-height (min (* h 0.9) (/ w 8))
+        inner-style {:border          "0.5px solid black"
+                     :border-top 0
+                     :border-bottom (str border-bottom "px solid black")
                      :display         "flex"
                      :justify-content "space-evenly"
                      :position "absolute"
                      :top 0
                      :left 0
+                     :width "100%"
+                     :height "100%"
                      :align-items     "center"}]
-    [:div {:style {:position "relative"}}
+    [:div {:style {:position "relative"
+                   :height          h
+                   :width           w}}
      [:div {:style (merge inner-style
                           {:z-index 9})}
-      (for [note notes]
-        [:div {:style (merge {:height        key-height
-                              :width         key-height
-                              :text-align "center"}
-                             (when note
-                               {:border        "1px solid black"
-                                :border-radius key-height
-                                :background    "lime"}))}
-         note])]
+      (for [[i note] (map-indexed vector notes)]
+        ^{:key i} [:div {:style (merge {:height        key-height
+                                        :width         key-height
+                                        :align-self "flex-end"
+                                        :display "flex"
+                                        :align-items "center"
+                                        :border 1
+                                        :justify-content "center"}
+                                       (when note
+                                         {:border        "1px solid black"
+                                          :border-radius key-height
+                                          :background    "lime"}))}
+                   [:p note]])]
      [:div {:style (merge inner-style {:order "0.5px solid black"})}
-      (for [_ (range marks)]
-        [:div {:style {:height        mark-height
-                       :width         mark-height
-                       :border        "1px solid black"
-                       :border-radius mark-height
-                       :background    "grey"}}])]]))
+      (for [i (range dots)]
+        ^{:key i} [:div {:style {:height        mark-height
+                                 :width         mark-height
+                                 :border        "1px solid black"
+                                 :border-radius mark-height
+                                 :background    "grey"}}])]]))
 
-(defn fret3 [sesh fnum]
-  (let [{:keys [fh] :as match}
-        @(r/track rule-react sesh {::fnum fnum})]
-    [fret 100 fh 1 nil]))
+(defn fret4 [sesh fret-id width height opts]
+  (let [{:keys [dots notes]}
+        @(r/track rule-react2
+                  ::fret4
+                  '[:in fid
+                    :what
+                    [fid :fret/notes notes]
+                    [fid :fret/dots dots]]
+                  sesh fret-id)]
+    [fret width height dots notes opts]))
 
-(comment
-  (doseq [_ (range 10)]
-    (println "."))
-  (rdom/render
-   [:div [fret3 sesh 0]
-    [fret3 sesh 1]]
-   js/document.body)
-  (o/query-all @sesh)
-  (swap! sesh o/fire-rules)
-  (swap! sesh insert-facts [[::fretboard {:fretboard/height 950}]])
-  99)
+(defn fretboard [sesh]
+  (let [{:keys [fret-seq width height]}
+        @(r/track rule-react2
+                  ::fretboard-render3
+                  '[:what
+                    [::fretboard :fretboard/width width]
+                    [::fretboard :fretboard/height height]
+                    [::fretboard :fretboard/fret-seq fret-seq]]
+                  sesh)
+        [ffret remaining] (split-at 2 (map-indexed vector fret-seq))
+        fret-count        (dec (count fret-seq))
+        ffret-height      (fret-height 1 fret-count)
+        [f1-id f2-id] (map second ffret)]
+    [:div {:style {}}
+     (concat
+      [^{:key f1-id} [fret4 sesh f1-id width (* 0.4 ffret-height height) {:no-border? true}]
+       ^{:key f2-id} [fret4 sesh f2-id width (* 0.7 ffret-height height)]]
+      (for [[i fret-id] remaining]
+        ^{:key fret-id} [fret4 sesh fret-id width (* height (fret-height i fret-count))]))]))
 
-(defn fret-2 [conn fret-num]
-  (let [[dots fbh fbw]
-        @(r/track reactq
-                  conn
-                  '[:find ?dots ?fbh ?fbw
-                    :in $ ?fret-num
-                    :where [?e :fret/number ?fret-num]
-                    [?e :fret/ndots ?dots]
-                    [?e2 :fretboard/height ?fbh]
-                    [?e2 :fretboard/width ?fbw]]
-                  :in [fret-num])]
-    [fret fbw (* fbh (fret-height (inc fret-num))) dots]))
+(split-at 2 '(1 2 3 4 5))
+(defn setting [sesh [id attr] current min max]
+  [:input {:style {:width 150}
+           :type "range"
+           :value current
+           :in min :max max
+           :on-change (fn [e]
+                        (let [newv (.. e -target -value)]
+                          (swap! sesh o/insert id attr (js/parseInt newv))
+                          (swap! sesh o/fire-rules)))}])
 
-(defn fretboard [conn]
-  (let [fret-nums @(r/track reactq
-                            conn
-                            '[:find ?fret-num
-                              :where [_ :fret/number ?fret-num]]
-                            :xform (comp cat
-                                         (x/sort)
-                                         (x/into [])))]
-    (prn fret-nums)
+(defn settings [sesh]
+  (let [{:keys [nfrets width height enabled-notes]}
+        @(r/track rule-react2
+                  ::settings2
+                  '[:what
+                    [::fretboard :fretboard/num-frets nfrets]
+                    [::fretboard :fretboard/width width]
+                    [::fretboard :fretboard/enabled-notes enabled-notes]
+                    [::fretboard :fretboard/height height]]
+                  sesh)]
+    [:div {:style {:float "left"}}
+     [:p "num frets: " nfrets]
+     [setting sesh [::fretboard :fretboard/num-frets] nfrets 3 40]
+     [:p "height: " height]
+     [setting sesh [::fretboard :fretboard/height] height 50 2000]
+     [:p "width: " width]
+     [setting sesh [::fretboard :fretboard/width] width 10 800]
+     [:div
+      (for [n notes]
+        ^{:key n}
+        [:div {:style {:display "inline-block"}}
+         [:div n]
+         [:input {:type "checkbox"
+                  :checked (boolean (enabled-notes n))
+                  :on-change (fn [e]
+                               (let [checked (.. e -target -checked)]
+                                 (swap! sesh o/insert
+                                        ::fretboard :fretboard/enabled-notes
+                                        ((if checked conj disj) enabled-notes n))
+                                 (swap! sesh o/fire-rules)))}]])]]))
+
+(defn tuners [sesh]
+  (let [{:keys [width strings]}
+        @(r/track rule-react2
+                  ::tuners
+                  '[:what
+                    [::fretboard :fretboard/string-seq strings]
+                    [::fretboard :fretboard/width width]]
+                  sesh)]
     [:div
-     (for [n fret-nums]
-       [fret-2 conn n])]))
+     (for [{:keys [id note]} strings]
+       ^{:key id}
+       [:select {:style {:width (/ width (count strings))}
+                 :on-change (fn [e]
+                              (let [newv (.. e -target -value)]
+                                (swap! sesh o/insert id :string/note newv)
+                                (swap! sesh o/fire-rules)))}
+        (for [n notes]
+          ^{:key n}
+          [:option {:value n
+                    :selected (= note n)}
+           n])])]))
 
 (defn init []
   (.log js/console "big wow")
   (rdom/render
-   [:div [fretboard conn]]
-   js/document.body))
+   [:div
+    [:div {:style {:float "left"
+                   :width "50%"
+                   :height "100%"}}
+     [tuners sesh]
+     [fretboard sesh]]
+    [settings sesh]]
+   js/document.body)
+
+  79)
 
 (comment
-  "boom"
-
-  (d/q '[:find ?number ?dots
-         :keys number dots
-         :where [?e :fret/number ?number]
-         [?e :fret/ndots ?dots]]
-       @conn)
-
-  (defn tester []
-    [:div (str "wenis " (->
-                         @(r/track ->reactq conn '[:find ?height
-                                                   :where [?e :db/ident :fretboard]
-                                                   [?e :fretboard/height ?height]]
-                                   cat)
-                         (doto prn)))])
-
+  (pprint/pprint
+   (o/query-all @sesh))
+  (doseq [_ (range 10)]
+    (println "."))
   (rdom/render
-   [:div
-    [fretboard conn]
-    [tester]
-    [:input {:style {:width "150px"}
-             :type "range"
-             :value (-> '[:find ?height
-                          :where [?e :db/ident :fretboard]
-                          [?e :fretboard/height ?height]]
-                        (d/q @conn)
-                        first first)
-             :min 1 :max 1000
-             :on-change (fn [e]
-                          (let [newv (.. e -target -value)]
-                            (d/transact! conn [{:db/ident :fretboard
-                                                :fretboard/height newv}])))}]]
-   js/document.body))
+   [:div {:style {:display "inline-block"}}
+    [fretboard sesh]
+    [settings sesh]]
+   js/document.body)
+  (o/query-all @sesh)
+  (do
+    (swap! sesh o/fire-rules)
+    nil)
+  (do
+    (swap! sesh insert-facts [[::fretboard {:fretboard/height 1250}]])
+    nil)
+  99)
